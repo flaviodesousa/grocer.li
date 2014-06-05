a21d72c234a88d81e91e86735106e45f1d301c2a
Flavio de Sousa <fl@viodesousa.com>
2014-06-05 02:21:07 -0300

    DB setup

---
+++ config/db.js
@@ -0,0 +1,23 @@
+var Bookshelf = require('bookshelf');
+
+Bookshelf.Conn = Bookshelf.initialize({
+  client: 'pg',
+  connection: '/var/run/postgresql grocerli'
+});
+
+Bookshelf.Conn.knex.schema.hasTable('users')
+  .then(function(exists) {
+    if(exists) return;
+
+    bspg.knex.schema.createTable('users', function(table) {
+      table.increments('id');
+      table.string('email').notNullable().unique();
+      table.string('secret').notNullable();
+    })
+    .then(function() {
+      console.log('users table created');
+    })
+    .catch(function(err) {
+      console.log('error: '+err);
+    });
+  });
