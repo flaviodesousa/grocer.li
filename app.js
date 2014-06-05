995be5acddd48f1ea3a00487cdef9b673bdb4ee5
Flavio de Sousa <fl@viodesousa.com>
2014-06-05 02:20:13 -0300

    Dependency management

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var load = require('express-load');
+
 var app = express()
+
+load('config')
+  .then('models')
+  .into(app);
 
 app.get('/?', function(req, res) {
 	console.log('Got a request!')
